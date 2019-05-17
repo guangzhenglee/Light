911f32489c7af055741ec9d998b36670cc3ed17f
esmusssein777 <380449395@qq.com>
2019-05-17 14:24:57 +0800

    部分的ServiceImpl层

--- light-system/src/main/java/com/light/system/service/impl/SysMenuServiceImpl.java
+++ light-system/src/main/java/com/light/system/service/impl/SysMenuServiceImpl.java
@@ -1,6 +1,7 @@
 package com.light.system.service.impl;
 
 import com.light.common.base.Ztree;
+import com.light.common.utils.StringUtil;
 import com.light.system.domain.SysMenu;
 import com.light.system.domain.SysRole;
 import com.light.system.domain.SysUser;
@@ -10,9 +11,7 @@ import com.light.system.service.ISysMenuService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * 菜单 业务层处理
@@ -28,24 +27,51 @@ public class SysMenuServiceImpl implements ISysMenuService {
     @Autowired
     private SysRoleMenuMapper roleMenuMapper;
 
+    /**
+     * 根据用户查询菜单
+     * @param user 用户信息
+     * @return 菜单列表
+     */
     @Override
     public List<SysMenu> selectMenusByUser(SysUser user) {
-        return null;
+        List<SysMenu> menus;
+        if (user.isAdmin()) {
+            menus = menuMapper.selectMenuAll();
+        } else {
+            menus = menuMapper.selectMenusByUserId(user.getUserId());
+        }
+        return menus;
     }
 
+    /**
+     * 查询菜单集合
+     * @return 所有菜单信息
+     */
     @Override
     public List<SysMenu> selectMenuList(SysMenu menu) {
-        return null;
+        return menuMapper.selectMenuList(menu);
     }
 
+    /**
+     * 查询菜单集合
+     * @return 所有菜单信息
+     */
     @Override
     public List<SysMenu> selectMenuAll() {
-        return null;
+        return menuMapper.selectMenuAll();
     }
 
+    /**
+     * 根据用户的ID查询权限
+     * @param userId 用户ID
+     * @return 权限列表
+     */
     @Override
     public Set<String> selectPermsByUserId(Long userId) {
-        return null;
+        List<String> perms = menuMapper.selectPermsByUserId(userId);
+        Set<String> permSet = new HashSet<>();
+        perms.stream().filter(StringUtil::isNotEmpty).forEach(perm -> permSet.addAll(Arrays.asList(perm.trim().split(","))));
+        return permSet;
     }
 
     @Override
